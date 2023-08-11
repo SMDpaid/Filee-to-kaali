a5df0681c1110f8541dd40e0b4c785ab7a53264d
SMDpaid <140572996+SMDpaid@users.noreply.github.com>
2023-08-11 15:06:27 +0530

    Update shortlink.py

--- shortlink.py
+++ shortlink.py
@@ -1,1 +1,30 @@
+import os
+import asyncio
+from os import getenv, environ
+from asyncio import TimeoutError
+from shortzy import Shortzy
+from Adarsh.vars import Var
 
+SHORTENER_API = str(getenv('SHORTENER_API', '1adcdddaef4dd9f90c9e967971b58bd52daff246'))
+SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'dalink.in'))
+
+
+shortzy = Shortzy(SHORTENER_API, SHORTENER_WEBSITE)
+
+async def get_shortlink(link):
+    if not SHORTENER_API or not SHORTENER_WEBSITE:
+        return link
+
+    try:
+        x = await shortzy.convert(link, silently_fail=True)
+    except Exception:
+        x = await get_shortlink_sub(link)
+    return x
+
+
+async def get_shortlink_sub(link):
+    url = f'https://{SHORTENER_WEBSITE}/api'
+    params = {'api': SHORTENER_API, 'url': link}
+    scraper = cloudscraper.create_scraper() 
+    r = scraper.get(url, params=params)
+    return r.json()["shortenedUrl"]
